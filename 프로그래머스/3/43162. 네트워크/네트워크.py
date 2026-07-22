b120b020b9d18cc1041f7cee8b0620ec2641c0d2
Yuni <90020627+young2ya@users.noreply.github.com>
2026-07-22 12:07:05 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 11.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,16 @@
+def solution(n, computers):
+    visited = [False] * n
+    
+    def dfs(node):
+        visited[node] = True
+        for i in range(n):
+            if computers[node][i] == 1 and not visited[i]:
+                dfs(i)
+                
+    count = 0
+    for i in range(n):
+        if not visited[i]:
+            dfs(i)
+            count += 1
+            
+    return count
